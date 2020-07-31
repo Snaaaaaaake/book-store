05968deadf1f0a684086e0d133b09bf4b7f84991
Snaaaaaaake <kissmyshinyass@mail.ru>
2020-07-31 15:10:34 +0300

    Updates

---
+++ precache-manifest.4ecfb0a50969ae554647908524d32e77.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d02fb8f64bd961edc8aed351e3cf84e",
+    "url": "/book-store/index.html"
+  },
+  {
+    "revision": "264cbde639d5d30fd3fd",
+    "url": "/book-store/static/js/2.544f5caa.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/book-store/static/js/2.544f5caa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5cc39a37dcbcd0dfdd34",
+    "url": "/book-store/static/js/main.c9eea748.chunk.js"
+  },
+  {
+    "revision": "02fb799921b397677b87",
+    "url": "/book-store/static/js/runtime-main.740a2d2a.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f45a930d2039241055389d54d4504d3b.js"
+  "/book-store/precache-manifest.4ecfb0a50969ae554647908524d32e77.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/book-store/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
